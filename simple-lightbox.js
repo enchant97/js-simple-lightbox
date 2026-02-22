d745b86c8b2ff21a4cac43f0d3565c4dfeedbab4
Leo Spratt <enchant97@users.noreply.github.com>
2026-02-22 16:30:40 +0000

    add initial image lightbox

--- simple-lightbox.js
+++ simple-lightbox.js
@@ -3,3 +3,54 @@
  * @license MIT
  */
 "use-strict";
+
+/**
+ * Setup lightbox, if there is any to setup
+ */
+function doSetup() {
+    /** @type {HTMLElement[]} */
+    const lightboxes = document.querySelectorAll("[data-lightbox]");
+    lightboxes.forEach((lightbox) => {
+        const lightboxType = lightbox.dataset["lightbox"];
+        if (lightboxType === "auto-discover") {
+            /** @type {HTMLImageElement[]} */
+            const imgs = lightbox.querySelectorAll("img");
+            for (const img of imgs) {
+                if (img.dataset["lightboxIsSetup"] === "true") { continue; }
+                setupImageLightbox(img);
+                img.dataset["lightboxIsSetup"] = "true";
+            }
+        }
+    });
+}
+
+/**
+ * Setup a lightbox for an image element
+ * @property {HTMLImageElement} imgElement - The image element to setup
+ */
+function setupImageLightbox(imgElement) {
+    imgElement.addEventListener("click", () => {
+        // remove existing lightboxes
+        document.body.querySelectorAll("#lightbox").forEach(v => v.remove());
+        // create lightbox
+        const baseContainer = document.createElement("dialog");
+        baseContainer.id = "lightbox";
+        baseContainer.addEventListener("close", () => baseContainer.remove());
+        const lightboxImg = document.createElement("img");
+        lightboxImg.src = imgElement.src;
+        baseContainer.appendChild(lightboxImg);
+        document.body.appendChild(baseContainer);
+        // show lightbox
+        baseContainer.showModal();
+    });
+}
+
+/**
+ * Ensure setup is always called,
+ * no matter how script is included
+ */
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", doSetup);
+} else {
+    doSetup();
+}
